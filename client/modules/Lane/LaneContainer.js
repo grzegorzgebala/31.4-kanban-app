d0833af763a635ce1a7400e730e8b89c416ac6b0
Grzegorz Gebala <grzegorz.gebala@yahoo.com>
2019-02-05 22:09:37 +0100

    Bug some in server_js

---
+++ client/modules/Lane/LaneContainer.js
@@ -0,0 +1,23 @@
+import { connect } from 'react-redux';
+import Lane from './Lane';
+import * as laneActions from './LaneActions';
+import { createNote } from '../Note/NoteActions';
+import { createLaneRequest, fetchLanes } from '../Lane/LaneActions';
+import { deleteLane, updateLane, editLane } from './LaneActions';
+import { createNoteRequest } from '../Note/NoteActions';
+
+
+const mapStateToProps = (state, ownProps) => ({
+  laneNotes: ownProps.lane.notes.map(noteId => state.notes[noteId])
+});
+
+const mapDispatchToProps = {
+  editLane,
+  deleteLane,
+  updateLane,
+  addNote: createNoteRequest,
+};
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Lane);
